f6eb642ce244f6285f6e06663f266c37bad832c3
anisha stha <stha.anisha16@gmail.com>
2024-03-01 23:24:44 +0545

    first commit

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php 
 
 function university_files(){
-    wp_enqueue_style('university_main_styles',get_stylesheet_uri());
+    wp_enqueue_style('university_main_styles');
 }
 add_action('wp_enqueue_scripts','university_files');
